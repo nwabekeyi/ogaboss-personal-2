56d4ef15e302f0030c547f8966b24fe8cb036630
Nwabeke <41685349+nwabekeyi@users.noreply.github.com>
2026-05-29 13:49:32 +0100

    Preserve autostack amount precision

--- src/modules/autostack/autostack.module.ts
+++ src/modules/autostack/autostack.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { QuidaxModule } from '../../infrastructure/providers/quidax/quidax.module';
+import { PaystackModule } from '../../infrastructure/providers/paystack';
+import { TransactionModule } from '../transaction/transaction.module';
 import { AutoStackController } from './controllers/autostack.controller';
 import { AutoStackService } from './services/autostack.service';
 
-@Module({ controllers: [AutoStackController], providers: [AutoStackService], exports: [AutoStackService] })
+@Module({
+  imports: [QuidaxModule, PaystackModule, TransactionModule],
+  controllers: [AutoStackController],
+  providers: [AutoStackService],
+  exports: [AutoStackService],
+})
 export class AutoStackModule {}
