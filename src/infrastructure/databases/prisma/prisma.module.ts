8109f6d6295097df6ee93f2df7f677d4c5ea9175
Nwabeke <41685349+nwabekeyi@users.noreply.github.com>
2026-06-10 16:06:47 +0100

    Update prisma.module.ts

--- src/infrastructure/databases/prisma/prisma.module.ts
+++ src/infrastructure/databases/prisma/prisma.module.ts
@@ -1,11 +1,10 @@
 // src/infrastructure/databases/prisma/prisma.module.ts
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { DbWatcherService } from './db-listerner.service';
 
 @Global()
 @Module({
-  providers: [PrismaService, DbWatcherService],
-  exports: [PrismaService, DbWatcherService],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
